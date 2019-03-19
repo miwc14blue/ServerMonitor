6db16a95fc53da5839790c3bfb3352a2231605df
Frank Dennissen <frank.dennissen@hva.nl>
2019-03-19 17:17:39 +0100

    Form and php to write a new user to the database. WORKS! However, no new user is created in the process.

--- API/createUser.php
+++ API/createUser.php
@@ -1,45 +1,36 @@
 <?php
-include_once("lib/DatabasePDO.php");
-//include_once("lib/User.php");
+include_once("../lib/DatabasePDO.php");
+// include_once("../lib/User.php");
 
 $databasePDOInstance = new DatabasePDO();
 $conn = $databasePDOInstance->get();
 
-    $userName => $_POST['userName'];
-    $firstName => $_POST['firstName'];
-    $lastName => $_POST['lastName']password_hash($password, PASSWORD_DEFAULT);,
-    $passWord => $_POST['password'];
-    $hash =>password_hash($password, PASSWORD_DEFAULT);
-    $role => "user";
-    $deleted => "0";
+// $userName = $_POST['userName'];
+// $firstName = $_POST['firstName'];
+// $lastName = $_POST['lastName'];
+// $passWord = $_POST['password'];
+// $hash = password_hash($password, PASSWORD_DEFAULT);
 
-$user = new User('userName');
-$data = [
+
+// $user = new User($userName, $firstName, $lastName, $hash);
+
+
+$user = [
     'userName' => $_POST['userName'],
     'firstName' => $_POST['firstName'],
-    'lastName' => $_POST['lastName']password_hash($password, PASSWORD_DEFAULT);,
-    'passWord' => $hash $_POST['password'],
+    'lastName' => $_POST['lastName'],
+    'hash' => password_hash($_POST['password'], PASSWORD_DEFAULT),
     'role' => 'user',
     'deleted' => '0'
-];  
-$query = "INSERT INTO orders (`userName`, `firstName`, `lastName`, `role`, `password`,`deleted`) 
-            VALUES (:userName, :firstName, :lastName, :role, :password, :deleted);";
+]; 
+$query = "INSERT INTO servermonitor.user (`userName`, `firstName`, `lastName`, `role`, `password`,`deleted`) 
+            VALUES (:userName, :firstName, :lastName, :role, :hash, :deleted);";
 
 try {
     $statement = $conn->prepare($query);
-    $statement->execute($data);
+    $statement->execute($user);
 } catch (PDOException $e){
     echo "Error: {$e->getMessage()}";
     
 }
 ?>
-
-
-function addUser($name, $email, $password){
-  $sql = "INSERT INTO `users` (`name`, `email`, `password`) VALUES (?,?,?)";
-  $this->stmt = $this->pdo->prepare($sql);
-  $hash = password_hash($password, PASSWORD_DEFAULT);
-  return $this->stmt->execute([$name, $email, $hash]);
-}
-
-$pass = addUser("John Doe", "john@doe.com", "password123");
--- lib/User.php
+++ lib/User.php
@@ -1,26 +1,19 @@
 <?php
 class User {
     
-    private $id
     private $userName;
     private $firstName;
     private $lastName;
     private $role;
-    private $password;
+    private $hash;
     private $deleted;
     
-    public function createUser() {
-        $user = 
-        $dsn = "{$this->driver}:dbname={$this->schema};port=3307;host={$this->host}";
-        
-        try {
-            $conn = new PDO($dsn, $this->username, $this->password);
-            return $conn;
-            
-        } catch (PDOException $e) {
-            echo "connection failed: {$e->getMessage()}";
+    public function createUser($userName, $firstName, $lastName, $hash) {
+        $this->userName = $userName;
+        $this->firstName = $firstName;
+        $this->hash = $hash;
+        $this->role = "user";
+        $this->deleted = "0";
         }
     }
-}
-
 ?>

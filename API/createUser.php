04a2e84371fa4be9bd17b8f379174588536fb39f
Frank Dennissen <frank.dennissen@hva.nl>
2019-03-20 11:06:11 +0100

    Update on create user file which enables an admin to make a new user and store it in the database.

--- API/createUser.php
+++ API/createUser.php
@@ -1,30 +1,40 @@
 <?php
 include_once("../lib/DatabasePDO.php");
-// include_once("../lib/User.php");
+include_once("../lib/User.php");
 
 $databasePDOInstance = new DatabasePDO();
 $conn = $databasePDOInstance->get();
 
-// $userName = $_POST['userName'];
-// $firstName = $_POST['firstName'];
-// $lastName = $_POST['lastName'];
-// $passWord = $_POST['password'];
-// $hash = password_hash($password, PASSWORD_DEFAULT);
+$userName = $_POST['userName'];
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$password1 = $_POST['password1'];
+$password2 = $_POST['password2'];
+$role = $_POST['role'];
+$hash = password_hash($password1, PASSWORD_DEFAULT);
 
-
-// $user = new User($userName, $firstName, $lastName, $hash);
+//TODO: this line of code should create a new user
+// private $user = new User($userName, $firstName, $lastName, $hash, $role);
 
 
 $user = [
-    'userName' => $_POST['userName'],
-    'firstName' => $_POST['firstName'],
-    'lastName' => $_POST['lastName'],
-    'hash' => password_hash($_POST['password'], PASSWORD_DEFAULT),
-    'role' => 'user',
+    'userName' => $userName,
+    'firstName' => $firstName,
+    'lastName' => $lastName,
+    'hash' => $hash,
+    'role' => $role,
     'deleted' => '0'
 ]; 
+
 $query = "INSERT INTO servermonitor.user (`userName`, `firstName`, `lastName`, `role`, `password`,`deleted`) 
             VALUES (:userName, :firstName, :lastName, :role, :hash, :deleted);";
+
+
+//TODO: This query should extract the parameters from the user and send it to the database. Does not function properly yet
+/*
+$query = "INSERT INTO servermonitor.user (`userName`, `firstName`, `lastName`, `role`, `password`,`deleted`) 
+            VALUES ($user->getUserName();, $user->getFirstName();, $user->getLastName();, $user->getRole();, $user->getHash();, $user->getDeleted(););";
+*/
 
 try {
     $statement = $conn->prepare($query);
--- lib/User.php
+++ lib/User.php
@@ -8,12 +8,38 @@ class User {
     private $hash;
     private $deleted;
     
-    public function createUser($userName, $firstName, $lastName, $hash) {
+    //constructor
+    public function __construct($userName, $firstName, $lastName, $hash, $role) {
         $this->userName = $userName;
         $this->firstName = $firstName;
         $this->hash = $hash;
-        $this->role = "user";
+        $this->role = $role;
         $this->deleted = "0";
+        echo "User made";
         }
+    
+    public function getUserName() {
+        return $this->userName;
     }
+    
+    public function getFirstName() {
+        return $this->firstName;
+    }
+
+    public function getLastName() {
+        return $this->lastName;
+    }
+    
+    public function getrole() {
+        return $this->role;
+    }
+    
+    public function gethash() {
+        return $this->hash;
+    }
+
+    public function getdeleted() {
+        return $this->deleted;
+    }
+}
 ?>

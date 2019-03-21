645ea53f5c6beb6afb0c4c9165a2044e255b4624
Arend <bitbucket.org@arend.33mail.com>
2019-03-21 14:21:00 +0100

    styles.css & databasePDO.php updated

--- lib/DatabasePDO.php
+++ lib/DatabasePDO.php
@@ -2,14 +2,16 @@
 class DatabasePDO {
     
     private $username = 'root';
-    private $password = 'root';
+    private $password = ''; 
+    // private $password = 'root'; 
     private $schema = 'servermonitor';
     private $driver = 'mysql';
-    private $host = '192.168.64.2';
+    private $host = '127.0.0.1';
+    // private $host = '192.168.64.2';
     
     public function get() {
         $dsn = "{$this->driver}:dbname={$this->schema};port=3307;host={$this->host}";
-        
+        // $dsn = "{$this->driver}:dbname={$this->schema};port=3306;host={$this->host}";
         try {
             $conn = new PDO($dsn, $this->username, $this->password);
             return $conn;

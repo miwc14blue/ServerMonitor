f54c1c8881c7786d1b7c408f27e9f38daf9d1aa4
Andy Valkenburg <andyvalkenburg@gmail.com>
2019-03-22 05:28:27 +0100

    DatabasePDO voor Arend aangepast en refinen userListOverview.php

--- html/userListOverview.php
+++ html/userListOverview.php
@@ -5,16 +5,6 @@ if(!isset($_SESSION['username']) || !($_SESSION['role']=='admin')){
 }
 ?>
 
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="utf-8">
-    <title>Gebruikers Overzicht</title>
-    <link rel="stylesheet" type="text/css" href="../css/styles.css">
-</head>
-
-<body>
 
 <!DOCTYPE html>
     <html>
@@ -24,7 +14,9 @@ if(!isset($_SESSION['username']) || !($_SESSION['role']=='admin')){
             Gebruikersoverzicht
         </title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="css/styles.css">
+        <link rel="stylesheet" href="../css/styles.css">
+        <link rel="stylesheet" href="../css/userlist-styles.css">
+        
     </head>
 
 
--- lib/DatabasePDO.php
+++ lib/DatabasePDO.php
@@ -1,32 +1,48 @@
 <?php
-class DatabasePDO {
+class DatabasePDO
+{
 
-    
+
     private $schema = 'servermonitor';
     private $driver = 'mysql';
     private $username = 'root';
-    private $password = ''; 
-    
-    private $host = '127.0.0.1'; 
+    private $password = '';
+
+    private $host = '127.0.0.1';
     private $port = '3307';
-    //voor Arend:
-    // private $host = '192.168.64.2';
-    // private $port = '3306';
-  
-    
-    
-    
-    public function get() {
-        $dsn = "{$this->driver}:dbname={$this->schema};port={$this->port};host={$this->host}";
 
-        try {
-            $conn = new PDO($dsn, $this->username, $this->password);
-            return $conn;
+    private $passwordMac = 'root';
+    private $hostMac = '192.168.64.2';
+    private $portMac = '3306';
 
-        } catch (PDOException $e) {
-            echo "connection failed: {$e->getMessage()}";
+
+
+
+    public function get()
+    {
+
+        if (PHP_OS_FAMILY == 'Mac') {
+
+            $dsn = "{$this->driver}:dbname={$this->schema};port={$this->portMac};host={$this->hostMac}";
+
+            try {
+                $conn = new PDO($dsn, $this->username, $this->passwordMac);
+                return $conn;
+            } catch (PDOException $e) {
+                echo "connection failed: {$e->getMessage()}";
+            }
+        } else {
+
+            $dsn = "{$this->driver}:dbname={$this->schema};port={$this->port};host={$this->host}";
+
+            try {
+                $conn = new PDO($dsn, $this->username, $this->password);
+                return $conn;
+            } catch (PDOException $e) {
+                echo "connection failed: {$e->getMessage()}";
+            }
         }
     }
 }
 
-?>
+ 

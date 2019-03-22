fdf4ee5b774ee542b70f5f2a9a05e7b9d2ba5955
Andy Valkenburg <andyvalkenburg@gmail.com>
2019-03-22 10:21:17 +0100

    like last commit, added security

--- html/makeUserForm.php
+++ html/makeUserForm.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if(!isset($_SESSION['username']) || !($_SESSION['role']=='user')){
+if(!isset($_SESSION['username']) || !($_SESSION['role']=='admin')){
    header("Location:../login.php");
 }
 ?>

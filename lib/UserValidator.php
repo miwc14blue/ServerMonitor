41b6ee892213ccde9f546a1a46b3b5eeae3af329
Frank Dennissen <frank.dennissen@hva.nl>
2019-03-24 14:32:19 +0100

    User validator created

--- lib/UserValidator.php
+++ lib/UserValidator.php
@@ -1,56 +1,119 @@
 <?php
 class UserValidator {
         
+/*-----------------------------------user name validaton---------------------------------*/
     public function validateUserName($data) {  
-        $errorCode = "";
+        $error = "";
         $userName = $data;
         
         if (empty($userName)){
-            $errorCode = "Gebruikersnaam is vereist.";
+            $error = "Gebruikersnaam is vereist.";
         }
         elseif (!preg_match("/^[a-zA-Z ]*$/",$userName)) {
-            $errorCode = "Alleen letters en spaties zijn toegestaan.";
+            $error = "Alleen letters en spaties zijn toegestaan.";
         }
-        $userNameAndErrCode = array($userName, $errorCode);
-        return $userNameAndErrCode;
+        $userNameAndError = array($userName, $error);
+        return $userNameAndError;
+    } 
+    
+/*-----------------------------------first name validaton---------------------------------*/ 
+    public function validateFirstName($data) {
+        $error = "";
+        $firstName = $data;
+        $firstNameTested = test_input($firstName);
+        
+        if (empty($firstName)){
+            $error = "Voornaam is vereist.";
         }
-    
-    
-    public function validateFirstName($firstName) {  
-        return $firstName;
+        elseif (!checkLength($firstName,2)) {
+            $error = "Voornaam moet uit ten minste 2 karakters bestaan.";
+        }
+        elseif ($firstNameTested !== $firstName){
+            $error = "De voornaam heeft ongelige karakters.";
+        }
+        $firstNameAndError = array($firstName, $error);
+        return $firstNameAndError;
+    }
+
+/*-----------------------------------last name validaton---------------------------------*/
+    public function validateLastName($data) {
+        $error = "";
+        $lastName = $data;
+        
+        if (empty($lastName)){
+            $error = "Achternaam is vereist";
+        }
+        elseif (!checkLength($lastName,2)) {
+            $error = "Achternaam moet uit ten minste 2 karakters bestaan.";
+        }
+        $lastNameAndError = array($lastName, $error);
+        return $lastNameAndError;
+    }
+
+/*-----------------------------------email address validaton---------------------------------*/      
+    public function validateEmail($data) {
+        $error = "";
+        $email = $data;
+        
+        if (empty($email)){
+            $error = "Email adres is vereist";
+        }
+        elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $error = "Ongeldig emailadres.";
+        }
+            $emailAndError = array($email, $error);
+            return $emailAndError;
     }
     
-    public function validateLastName($lastName) {  
-        return $lastName;
+/*-----------------------------------password validaton---------------------------------*/ 
+    public function validatePassword($data1, $data2) {
+        $error = "";
+        $password1 = $data1;
+        $password2 = $data2;
+        
+        if (empty($password1 or $password2)){
+            $error = "Wachtwoord is vereist.";
+        } 
+        elseif($password1!== $password2){
+            $error="Wachtwoorden zijn niet gelijk";
+        }
+        elseif(!checkPassword($password1)){ // checkPassword returns true if patern is fine if not it will return false
+                                            // and !false will be true so will create error massage
+            $error="Het wachtwoord moet bestaan uit (minimaal) 8 tekens, waarvan (minimaal) 1 hoofdletter, 1 kleine letter en 1 nummer.";
+        }
+            $passwordAndError = array($password1, $error);
+            return $passwordAndError;
     }
     
-    public function validateEmailAddress($emailAddress) {  
-        return $emailAddress;
-    }    
-    
-    public function validatePassword($password1, $password2) {  
-        return $password1;
+/*-----------------------------------role validaton---------------------------------*/ 
+    public function validateRole($data) {
+        $error = "";
+        $role = $data;
+        
+        if (empty($role)){
+            $error = "Rol is vereist.";
+        }
+        $roleAndError = array($role, $error);
+        return $roleAndError;
     }
-    
-    public function validateRole($role) {  
-    return $role;
-    }
-    
+
+/*-----------------------------helper functions-----------------------------------*/    
+    //this function length of the input  for firstname,lastname or whereever required
     function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
     }
     
-    ///this function length of the input  for firstname,lastname or whereever required
+    //this function length of the input  for firstname,lastname or whereever required
     function checkLength($str, $len){
-    return strlen($str) >= $len;
+        return strlen($str) >= $len;
     }
 
     // check pasword with regex  due to requirements
-    function checkPassword($pass){
-        return preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/', $pass);
+    function checkPassword($password){
+        return preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/', $password);
     }
 }
 

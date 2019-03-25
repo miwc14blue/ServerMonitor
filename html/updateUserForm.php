22f86d76218036dcbd125b0cfc4092107ff0b050
Niek van de Ven <niek.van.de.ven@hva.nl>
2019-03-25 17:11:36 +0100

    nav in updateuserform.php vervangen met de include voor menu.php

--- html/updateUserForm.php
+++ html/updateUserForm.php
@@ -18,79 +18,70 @@ if(!isset($_SESSION['username']) || !($_SESSION['role']=='admin')){
     </head>
 
     <body>
-
-        <nav id="navHeader">
-            <img class="logo" src="../img/logo.png">
-            <a href="systeemOverzichtAdm.php">Systeem Overzicht</a>
-            <a class="active" href="userListOverview.php">Gebruikers</a>
-            <a class="uitloggen" href="../API/logout.php">Uitloggen</a>
-        </nav>
+      <?php
+          require_once('menu.php');
+      ?>
 
         <div class="form-container">
           <h1>Gebruiker bewerken</h1>
 
-        <form method="POST" action="../API/createUser.php">
-            <div id="fields">
-                <p>
-                    <label>Gebruikersnaam
-                        <input name='userName' type="text" class="input"/>
-                    </label>
-                </p>
-                
-                <div id="containerLinks">
-                    <p class="next">
-                        <label>Wachtwoord
-                            <input name='password1' type="text" class="input"/>
-                        </label>    
-                    </p>
-                    <p>
-                        <label>Voornaam
-                            <input name='firstName' type="text" class="input1"/>
-                        </label>    
-                    </p>
-                    <p>
-                        <label>Rol
-                            <input list="rol" name="Rol" class="input"/>
-                            <datalist id="rol">
-                                <option value="User">
-                                <option value="Administrator">
-                            </datalist>
-                        </label>
-                    </p> 
-                </div>
-                
-                
-                <div id="containerRechts">
-                    <p id="rechts" >
-                        <label>Achternaam
-                            <input class="rechts" name='lastName' type="text" class="input1"/>
-                        </label>    
-                    </p>
-                    <p id="rechts" class="next">
-                        <label>Herhaal wachtwoord
-                            <input name='password2' type="text" class="input"/>
-                        </label>    
-                    </p>
-                    <p id="rechts" class="next">
-                        <label>Email-adres
-                            <input name='email' type="text" class="input"/>
-                        </label>    
-                    </p>
-                </div>
-                        
-               
-            </div>
-            
-            <div id="buttons">
-                <button id="cancelbutton" class="buttons" onclick="location.href=`userListOverview.php`"> Annuleren</button>
-                <input type="submit" value="Gebruiker verwijderen" class="buttons" id="buttenGVerwijderen" />    
+          <form method="POST" action="../API/createUser.php">
+              <div id="fields">
+                  <p>
+                      <label>Gebruikersnaam
+                          <input name='userName' type="text" class="input"/>
+                      </label>
+                  </p>
 
-                <input type="submit" value="Gebruiker opslaan" class="buttons" id="buttenGOpslaan" />
-            </div>
-            
+                  <div id="containerLinks">
+                      <p class="next">
+                          <label>Wachtwoord
+                              <input name='password1' type="text" class="input"/>
+                          </label>
+                      </p>
+                      <p>
+                          <label>Voornaam
+                              <input name='firstName' type="text" class="input1"/>
+                          </label>
+                      </p>
+                      <p>
+                          <label>Rol
+                              <input list="rol" name="Rol" class="input"/>
+                              <datalist id="rol">
+                                  <option value="User">
+                                  <option value="Administrator">
+                              </datalist>
+                          </label>
+                      </p>
+                  </div>
 
-        </form>    
-        </div>    
+                  <div id="containerRechts">
+                      <p id="rechts" >
+                          <label>Achternaam
+                              <input class="rechts" name='lastName' type="text" class="input1"/>
+                          </label>
+                      </p>
+                      <p id="rechts" class="next">
+                          <label>Herhaal wachtwoord
+                              <input name='password2' type="text" class="input"/>
+                          </label>
+                      </p>
+                      <p id="rechts" class="next">
+                          <label>Email-adres
+                              <input name='email' type="text" class="input"/>
+                          </label>
+                      </p>
+                  </div>
+              </div>
+
+              <div id="buttons">
+                  <button id="cancelbutton" class="buttons" onclick="location.href=`userListOverview.php`"> Annuleren</button>
+                  <input type="submit" value="Gebruiker verwijderen" class="buttons" id="buttenGVerwijderen" />
+
+                  <input type="submit" value="Gebruiker opslaan" class="buttons" id="buttenGOpslaan" />
+              </div>
+          </form>
+        </div>
     </body>
 
     </html>

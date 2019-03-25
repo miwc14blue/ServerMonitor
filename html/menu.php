564e6475038c61bc8efd59641dd2de18aaecf480
Niek van de Ven <niek.van.de.ven@hva.nl>
2019-03-25 13:41:13 +0100

    als gebruiker een user is, dan geen menu knoppen behalve uitloggen

--- html/menu.php
+++ html/menu.php
@@ -1,7 +1,7 @@
 <nav id="navHeader">
   <img class="logo" src="../img/logo.png">
-  <a class="<?php echo ($_SERVER['PHP_SELF'] == '/ServerMonitor/html/systeemOverzichtAdm.php') ? 'active':''; ?>" href="systeemOverzichtAdm.php">Systeem overzicht</a>
   <?php if($_SESSION['role']=='admin'){ ?>
+    <a class="<?php echo ($_SERVER['PHP_SELF'] == '/ServerMonitor/html/systeemOverzichtAdm.php') ? 'active':''; ?>" href="systeemOverzichtAdm.php">Systeem overzicht</a>
     <a class="<?php echo ($_SERVER['PHP_SELF'] == '/ServerMonitor/html/userListOverview.php') ? 'active':''; ?>" href="userListOverview.php">Gebruikers</a>
   <?php } ?>
   <a class="uitloggen" href="../API/logout.php">Uitloggen</a>

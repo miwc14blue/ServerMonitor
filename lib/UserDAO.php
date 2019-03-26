24a0f6eb73435039cd0f02db06550fc820d8be28
Andy Valkenburg <andyvalkenburg@gmail.com>
2019-03-26 11:25:31 +0100

    deleted old includes in login

--- lib/UserDAO.php
+++ lib/UserDAO.php
@@ -1,6 +1,5 @@
 <?php
 include 'DAO.php';
-include("../API/CreateUserForm.php");
 
 class UserDAO extends DAO {  
     
@@ -26,22 +25,6 @@ class UserDAO extends DAO {
         return $user;
     }
 
-    public function createUserFromDB($userName) {
-        $query = "SELECT * from user where userName= '$userName';";
-        $userJSON = parent::SendQueryToDB($query);
-        $data = json_decode($userJSON, true);
-        $user = new User($data['userName'], $data['firstName'], $data['lastName'], $data['email'], $data['password'], $data['role']);
-        $user->setDeleted($data['deleted']);
-        var_dump($data);
-        die;
-        return $user;
-    }
-
-    public function loginInfo($userName){
-        $query = "SELECT password, role, deleted from user where userName= '$userName';";
-        $hashAndDelete = parent::SendQueryToDB($query);
-        return $hashAndDelete;
-    }
     
     public function retrieveUserList() {
         $query = "SELECT userName, firstName, lastName, role 
@@ -56,7 +39,7 @@ class UserDAO extends DAO {
          $query = "SELECT userName, firstName, lastName, role 
         FROM servermonitor.user WHERE userName= '$userName';";
         $user = parent::SendQueryToDB($query);
-        $editUserForm = new UserForm($user);
+       //TODO send to somwhere for coming into the form.
     }
 }
 ?>

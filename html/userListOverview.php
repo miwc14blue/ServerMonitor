2945acaab7e4c24ddd07e24fa20aaea0db2accce
Arend <bitbucket.org@arend.33mail.com>
2019-03-28 16:06:38 +0100

    fixed backtick  userLIstOverview, row 68

--- html/userListOverview.php
+++ html/userListOverview.php
@@ -65,11 +65,10 @@ if (!isset($_SESSION['username']) || !($_SESSION['role'] == 'admin')) {
                     </td>
 
                     <td>
-                        <a href="../API/UserEdit.php?userName=<?php echo $user->userName ?>'"><i class="fa fa-pencil"> </i></a>
+                        <a href="../API/UserEdit.php?userName=<?php echo $user->userName ?>"><i class="fa fa-pencil"> </i></a>
 <!--                TODO: Either modify the anchor or style the button please--> 
                         
-                        <button id="btn"> <i class="fa fa-pencil" 
-                        onclick="window.location.href='../API/UserEdit.php?userName=<?php echo $user->userName ?>'"> </i>  </button>
+                        <button id="btn" onclick="window.location.href='../API/UserEdit.php?userName=<?php echo $user->userName ?>'"><i class="fa fa-pencil" > </i>  </button>
 
                         <!--                    Trigger/Open The Modal -->
                         <a class="trash" onclick="show('<?php echo $user->userName ?>')" <?php if ($_SESSION['username'] == $user->userName) {
